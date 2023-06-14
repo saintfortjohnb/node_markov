55fd3a64c8e931aac2e4d408d699705ab923765a
John Saint-Fort <johnsaint-fort@Johns-MacBook-Pro.local>
2023-06-13 23:38:51 -0400

    final

--- makeText.js
+++ makeText.js
@@ -1,2 +1,56 @@
 /** Command-line tool to generate Markov text. */
+const fs = require('fs');
+const axios = require('axios');
+
+const MarkovMachine = require('./markov');
+
+function readFile(path) {
+  try {
+    return fs.readFileSync(path, 'utf-8');
+  } catch (error) {
+    console.error(`Error reading file: ${path}`);
+    process.exit(1);
+  }
+}
+
+async function readURL(url) {
+  try {
+    const response = await axios.get(url);
+    return response.data;
+  } catch (error) {
+    console.error(`Error fetching URL: ${url}`);
+    process.exit(1);
+  }
+}
+
+function generateText(input) {
+  const mm = new MarkovMachine(input);
+  return mm.makeText();
+}
+
+async function run() {
+  const [type, source] = process.argv.slice(2);
+
+  let input;
+
+  if (type === 'file') {
+    input = readFile(source);
+  } else if (type === 'url') {
+    input = readURL(source);
+  } else {
+    console.error('Invalid input type. Please specify "file" or "url".');
+    process.exit(1);
+  }
+
+  try {
+    const text = await input;
+    const generatedText = generateText(text);
+    console.log(generatedText);
+  } catch (error) {
+    console.error(error.message);
+    process.exit(1);
+  }
+}
+
+run();
 
--- markov.js
+++ markov.js
@@ -18,6 +18,21 @@ class MarkovMachine {
 
   makeChains() {
     // TODO
+    const chains = {};
+    const { words } = this;
+
+    for (let i = 0; i < words.length; i++) {
+      const word = words[i];
+      const nextWord = words[i + 1] || null;
+
+      if (!chains[word]) {
+        chains[word] = [];
+      }
+
+      chains[word].push(nextWord);
+    }
+
+    this.chains = chains;
   }
 
 
@@ -25,5 +40,19 @@ class MarkovMachine {
 
   makeText(numWords = 100) {
     // TODO
+    const { chains } = this;
+    const words = Object.keys(chains);
+    let currentWord = words[Math.floor(Math.random() * words.length)];
+    let text = [];
+
+    while (text.length < numWords && currentWord !== null) {
+      text.push(currentWord);
+      const possibleNextWords = chains[currentWord];
+      currentWord = possibleNextWords[Math.floor(Math.random() * possibleNextWords.length)];
+    }
+
+    return text.join(" ");
   }
 }
+
+module.exports = MarkovMachine;
